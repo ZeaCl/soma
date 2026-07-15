135feba33e34019e0bf91b115e1ce46e1615c6a7
Carlos Hinostroza <chinostroza@gmail.com>
2026-07-14 21:41:36 -0400

    fix: allow POST method in CORS for file upload Closes #218 (#56)

--- lib/soma_web/endpoint.ex
+++ lib/soma_web/endpoint.ex
@@ -1,7 +1,7 @@
 defmodule SomaWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :soma
 
-  plug Corsica, origins: "*", allow_headers: ["authorization", "content-type", "x-api-key", "x-zea-org-id"]
+  plug Corsica, origins: "*", allow_headers: ["authorization", "content-type", "x-api-key", "x-zea-org-id"], allow_methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
   plug Plug.Parsers, parsers: [:json], json_decoder: Jason
